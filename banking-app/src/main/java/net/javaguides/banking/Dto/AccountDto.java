08ebcaaea8f7e1d49ef9674478dba653688590ae
Sachin H M <hmsachin300@gmail.com>
2026-04-02 19:16:10 +0530

    Initial commit

---
+++ banking-app/src/main/java/net/javaguides/banking/Dto/AccountDto.java
@@ -0,0 +1,15 @@
+package net.javaguides.banking.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AccountDto {
+    private Long id;
+    private String accountHolderName;
+    private double balance;
+
+
+
+}
